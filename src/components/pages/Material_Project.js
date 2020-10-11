3fad2a6f4ef1085c86eff7ece91855186014c5c5
pradeepkrishna1409 <pradeep2444@gmail.com>
2020-10-10 22:05:13 -0700

    Update Material_Project.js

--- src/components/pages/Material_Project.js
+++ src/components/pages/Material_Project.js
@@ -1,4 +1,5 @@
 import MaterialTable from 'material-table';
+import Table from '@material-ui/core/Table';
 import Container from '@material-ui/core/Container';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
@@ -19,64 +20,87 @@ import Remove from '@material-ui/icons/Remove';
 import SaveAlt from '@material-ui/icons/SaveAlt';
 import Search from '@material-ui/icons/Search';
 import ViewColumn from '@material-ui/icons/ViewColumn';
+import Loader from 'react-loader-spinner'
+import 'react-notifications/lib/notifications.css';
+import { NotificationManager } from 'react-notifications';
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import 'moment-timezone';
+import { TablePagination } from "@material-ui/core";
 
 
-export default function MaterialTableDemo() {
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'Project', field: 'Project' },
-      { title: 'Product_Family', field: 'Program' },
-      { title: 'Device_Type', field: 'Device Type' },
-      {
-        title: 'AME_Engagement',
-        field: 'AME Engagement',
-        value: "High",
-        lookup:
-        {
-          "High": "High",
-          "Low": "Low"
-        },
-      },
-      { title: 'Product Assessment Date', field: 'Product Assessment Date', type: "date" },
 
-      { title: 'BRD Date', field: 'BRD Date', type: "date" },
+export default function MaterialProject({ children, Pagesize , SetPagesize, searchTerm , setsearchTerm}) {
+  const [state, setState] = React.useState([]);
+  const [dataFetched, setDataFetched] = useState(false);
+  //const [rowsPerPage, setRowsPerPage] = useLocalStorage("page_set", 20);
 
-      { title: 'Proto Date', field: 'Proto Date', type: "date" },
 
-      { title: 'Dev Commit Date', field: 'Dev Commit Date', type: "date" },
+  const handleChangeRowsPerPage = event => {SetPagesize(+event.target.value); console.log( 'page size = ', +event.target.value );}; 
 
-      { title: 'HVT Date', field: 'HVT Date', type: "date" },
+  console.log( 'rendering material project; page size = ', Pagesize );
 
-      { title: 'EVT Date', field: 'EVT Date', type: "date" },
-
-      { title: 'DVT Date', field: 'DVT Date', type: "date" },
-
-      { title: 'PVT Date', field: 'PVT Date', type: "date" },
-
-      { title: 'MTE Actual', field: 'MTE Actual' },
-
-      { title: 'MTM Actual', field: 'MTM Actual' },
-    ],
-    data :[]
-  });
 
   useEffect(() => {
     loadUsers();
-  },[]);
+    console.log(" use effect called ")
+  }, []);
+
+
+  let options_set= {
+    addRowPosition: "first",
+    pageSize: Pagesize,
+    pageSizeOptions: [5, 10, 20, 30, 50, 75, 100],
+    toolbar: true,
+    paging: true,
+    searchFieldStyle: {
+      width: "24ch",
+      color: "red"
+    },
+    searchText:searchTerm,
+    searchFieldAlignment : "right",
+    headerStyle: {
+      backgroundColor: '#01579b',
+      color: '#FFF',
+      position: 'sticky', top: 0 
+
+    },
+    filtering: true,
+    maxBodyHeight: '1500px' 
+  }
+
+  console.log( 'options_set = ', options_set );
 
 
   const loadUsers = async () => {
 
     const result = await axios.get("/headcount/project_schedule");
     console.log(" user is ", result.data)
-    setState(state => ({ columns: state.columns, data: result.data }
-    ));
+    setState(result.data)
+
+    setDataFetched(true)
+
+    const pf = await axios.get("/headcount/product_family");
+    console.log(" type pf is ", typeof pf.data)
+    console.log(" pf is ", pf.data)
+
+    var pf_res = pf.data.reduce(function (map, obj) {
+      console.log("obj is ", obj)
+      map[obj.program_name] = obj.program_name;
+      return map;
+    }, {});
+
+    console.log("pf_res is ", pf_res)
+
   };
 
-
-
   const tableIcons = {
-    Add: () => 'Add Project',
+    //Add: () => 'Add Project',
+    Add: forwardRef((props, ref) => (
+      <Fab ref={ref} {...props} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+    )),
     Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
     Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
@@ -96,113 +120,258 @@ export default function MaterialTableDemo() {
   };
 
 
+  const toolbarStyle = {
+    minHeight: '80px',
+  };
+
+  const moment = require('moment');
+  moment().tz("America/Los_Angeles").format();
+
 
   return (
 
-    <Container maxWidth="lg">
-      <MaterialTable
-        localization={{
-          pagination: {
-            labelDisplayedRows: '{from}-{to} of {count}'
-          },
-          toolbar: {
-            nRowsSelected: '{0} row(s) selected'
-          },
-          header: {
-            actions: 'Modify'
-          },
-          body: {
-            emptyDataSourceMessage: 'No records to display',
-            filterRow: {
-              filterTooltip: 'Filter'
-            },
-            addTooltip: '+'
-          }
-        }}
-        options={{
-          addRowPosition: "first",
-          pageSize: 20,
-          pageSizeOptions: [5, 10, 20, 30 ,50, 75, 100 ],
-          toolbar: true,
-          paging: true,
-          headerStyle: {
-            backgroundColor: '#01579b',
-            color: '#FFF'
-          },
+    <div style={{ marginLeft: 0 }}>
+    <Container maxWidth="false">
+
+    <div>
+      {!dataFetched ? <Loader type="Puff" color='Teal'  style={{ width:"1" , position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }} /> :
+        <div style={{ maxWidth: "100%", paddingTop: "12px" }}>
+          <MaterialTable  
+          toolbar = {{
+            minHeight: '10px'
           }}
-        icons={tableIcons}
-        title="Projects"
-        columns={state.columns}
-        data={state.data}
-        editable={{
-          onRowAdd: (newData) => {
-            console.log(" newData is ", newData);
-
-            return axios.post(
-              `/headcount/project_schedule_ins/`,
-              newData,
-              {
-                headers: {
-                  'Content-Type': 'application/json'
-                }
+            localization={{
+              pagination: {
+                labelDisplayedRows: '{from}-{to} of {count}'
+              },
+              toolbar: {
+                nRowsSelected: '{0} row(s) selected'
+              },
+              header: {
+                actions: 'Modify'
+              },
+              body: {
+                emptyDataSourceMessage: 'No records to display',
+                filterRow: {
+                  filterTooltip: 'Filter'
+                },
+                addTooltip: '+'
               }
-            )
-              .then(resp => {
-                setState((prevState) => {
-                  const data = [...prevState.data];
-                  data.push(newData);
-                  return { ...prevState, data };
-                })
-              })
-              .catch(error => alert(error.message))
-          },
-
-          onRowUpdate: (newData, oldData) => {
-
-            console.log(" newData is ", newData);
-            console.log(" oldData is ", oldData);
-
-            return axios.put(
-              `/headcount/project_schedule_upd/${oldData.Project}`,
-              newData,
+            }}
+            components={{
+              Pagination: props => (
+                <TablePagination
+                  {...props}
+                  rowsPerPageOptions={[5, 10, 20, 30, 50, 75, 100]}
+                  rowsPerPage={Pagesize}
+                  //updates pagination, but no re-size
+                  onChangeRowsPerPage={handleChangeRowsPerPage}
+                />
+              )
+            }}
+            options={options_set}
+            icons={tableIcons}
+            title="Project Schedule"        
+            columns={[
+              { title: 'Project', field: 'Project', editable: 'onAdd' },
               {
-                headers: {
-                  'Content-Type': 'application/json'
+                title: 'Product Family', field: 'Product_Family', lookup: {
+                  "Advanced Products": "Advanced Products",
+                  "E-Readers": "E-Readers",
+                  "Echo": "Echo",
+                  "Emerging & Jumpstart": "Emerging & Jumpstart",
+                  "Fire TV": "Fire TV",
+                  "Health & Wellness": "Health & Wellness",
+                  "Tablets": "Tablets",
+                  "N/A" : "N/A"
                 }
-              }
-            )
-              .then(resp => {
-                if (oldData) {
-                  setState((prevState) => {
-                    const data = [...prevState.data];
-                    data[data.indexOf(oldData)] = newData;
-                    return { ...prevState, data };
-                  });
+              },
+              {
+                title: 'Device Type', field: 'Device_Type', lookup: {
+                  "Accessory": "Accessory",
+                  "Device": "Device",
+                  "N/A" : "N/A"
                 }
-              })
-              .catch(error => alert(error.message))
+              },
+              {
+                title: 'AME Engagement',
+                field: 'AME_Engagement',
+                lookup: {
+                  "High": "High",
+                  "Low": "Low",
+                  "Medium": "Medium",
+                  "Fixed" : "Fixed",
+                  "N/A" : "N/A"
+                }
+              },
+              { title: 'Street Date', field: 'Street_Date', type: "date", filtering: false  },
+              { title: 'Product Assessment Date', field: 'Product_Assessment_Date', type: "date", filtering: false },
+
+              { title: 'BRD Date', field: 'BRD_Date', type: "date" , filtering: false},
+
+              { title: 'Proto Date', field: 'Proto_Date', type: "date" , filtering: false},
+
+              { title: 'Dev Commit Date', field: 'Dev_Commit_Date', type: "date", filtering: false },
+
+              { title: 'HVT Date', field: 'HVT_Date', type: "date", filtering: false },
+
+              { title: 'EVT Date', field: 'EVT_Date', type: "date", filtering: false },
+
+              { title: 'DVT Date', field: 'DVT_Date', type: "date" , filtering: false},
+
+              { title: 'PVT Date', field: 'PVT_Date', type: "date", filtering: false },
+
+              { title: 'MTE Plan', field: 'MTE_Actual', filtering: false },
+
+              { title: 'MTM Plan', field: 'MTM_Actual', filtering: false },
+
+              { title: 'MDE Plan', field: 'MDE_Actual', filtering: false },
+
+              { title: 'MTS Plan', field: 'MTS_Actual', filtering: false },
+            ]}
+            data={state}
+            editable={{
+              onRowAdd: (newData) => {
+                console.log(" newData is ", newData);
+
+                return axios.post(
+                  `/headcount/project_schedule`,
+                  newData,
+                  {
+                    headers: {
+                      'Content-Type': 'application/json'
+                    }
+                  }
+                )
+                  .then(resp => {
+                    setState((prevState) => {
+                      const newProducts = [...state];
+                      
+                      if (newData.AME_Engagement == 'High'  &&  newData.program == 'Jumpstart') {
+
+                        newData.MTE_Actual = "1"
+                        newData.MTM_Actual = "1"
+                        newData.MTS_Actual = "0"
+                        newData.MDE_Actual = "0.5"
+
+                      }
+
+                      else if (newData.AME_Engagement == 'Medium'  &&  newData.program == 'Jumpstart') {
+
+                        newData.MTE_Actual = "0.75"
+                        newData.MTM_Actual = "1"
+                        newData.MTS_Actual = "0"
+                        newData.MDE_Actual = "0.5"
+
+                      }
+
+                      else if (newData.AME_Engagement == 'Low'  &&  newData.program == 'Jumpstart') {
+
+                        newData.MTE_Actual = "0.15"
+                        newData.MTM_Actual = "0.25"
+                        newData.MTS_Actual = "0"
+                        newData.MDE_Actual = "0.01"
+
+                      }
+
+                      else if (newData.AME_Engagement == 'Medium'  &&  newData.program != 'Jumpstart') {
+
+                        newData.MTE_Actual = "1.50"
+                        newData.MTM_Actual = "1.50"
+                        newData.MTS_Actual = "0.5"
+                        newData.MDE_Actual = "1.50"
+
+                      }
+
+                      else if (newData.AME_Engagement == 'High'  &&  newData.program != 'Jumpstart') {
+
+                        newData.MTE_Actual = "2"
+                        newData.MTM_Actual = "2"
+                        newData.MTS_Actual = "1"
+                        newData.MDE_Actual = "2"
 
 
-          },
-          onRowDelete: (oldData) => {
-            console.log(" oldData is ", oldData);
+                      }
 
-            return axios.delete(
-              `/headcount/project_schedule_del/${oldData.Project}`
-            )
-              .then(resp => {
-                if (oldData) {
-                  setState((prevState) => {
-                    const data = [...prevState.data];
-                    data.splice(data.indexOf(oldData), 1);
-                    return { ...prevState, data };
-                  });
-                }
-              })
-              .catch(error => alert(error.message))
-          },
-        }}
-      />
-    </Container>
+                      else if (newData.AME_Engagement == 'Low'  &&  newData.program != 'Jumpstart') {
+
+                        newData.MTE_Actual = "0.50"
+                        newData.MTM_Actual = "1"
+                        newData.MTS_Actual = "0.25"
+                        newData.MDE_Actual = "0.50"
+
+                      }
+
+
+                      newProducts.push(newData);
+                      
+                      setState([...newProducts]);
+                    })
+
+                    NotificationManager.success(`${newData.Project} Project was added`, 'Successful!', 5000);
+
+                  })
+                  .catch(error => alert(error.message))
+              },
+
+              onRowUpdate: (newData, oldData) => {
+
+                console.log(" newData is ", newData);
+                console.log(" oldData is ", oldData);
+
+                return axios.put(
+                  `/headcount/project_schedule`,
+                  newData,
+                  {
+                    headers: {
+                      'Content-Type': 'application/json'
+                    }
+                  }
+                )
+                  .then(response => {
+                    if (oldData) {
+                      let product = oldData
+
+                      setState((prevState) => {
+                        const newProducts = [...state];
+                        const index = product.tableData.id;
+                        newProducts[newProducts.indexOf(oldData)] = newData;
+                        setState([...newProducts]);
+                      });
+                    }
+
+                    NotificationManager.success(`${oldData.Project} Project was edited`, 'Successful!', 2000);
+
+                  })
+                  .catch(error => alert(error.message))
+
+
+              },
+              onRowDelete: (oldData) => {
+                console.log(" oldData is ", oldData);
+                let product = oldData
+                return axios.delete(`/headcount/project_schedule_del/${oldData.Project}`)
+                  .then(response => {
+                    const newProducts = [...state];
+                    const index = product.tableData.id;
+                    newProducts.splice(index, 1);
+                    setState([...newProducts]);
+                    NotificationManager.success(`${oldData.Project} Project was deleted`, 'Successful!', 2000);
+                  })
+                  .catch(error => {
+                    alert(error.message);
+                  })
+
+              },
+            }}
+          />
+        </div>
+      }
+      </div>
+      </Container>
+      </div>
+
+
   );
 }
+
